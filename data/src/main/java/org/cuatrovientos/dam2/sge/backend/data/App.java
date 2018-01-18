b473f7f01835b324112a7c06916a5b6fb8b4f6ef
David <David@DESKTOP-KC1PK8P>
2018-01-18 12:44:44 +0100

    V4. Algún comentario

--- data/src/main/java/org/cuatrovientos/dam2/sge/backend/data/App.java
+++ data/src/main/java/org/cuatrovientos/dam2/sge/backend/data/App.java
@@ -2,10 +2,6 @@ package org.cuatrovientos.dam2.sge.backend.data;
 
 import org.cuatrovientos.dam2.sge.backend.data.model.Customer;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
@@ -13,6 +9,6 @@ public class App
         System.out.println( "Hello World!" );
         Customer myCustomer = new Customer("David", "Perez", "david@perez.com");
         
-        //aquí tenemos que probar el método Create
+        //aquí tenemos que probar el método Create. Y esto es para hacer commit again
     }
 }
